7ad3faf269f3a8c1d12c5590276ef303bf6a5a3f
DarwinAlvarado <dwjrafk@gmail.com>
2024-03-12 18:15:31 -0500

    mi primera confirmación de actualización

--- script.js
+++ script.js
@@ -13,21 +13,21 @@
 
 //SELECIONAR ELEMENTOS
 
-let contenidoTitulo = "Acerca de mí";  //con cualquier otro titulo no se cumple
+//let contenidoTitulo = "Acerca de mí";  //con cualquier otro titulo no se cumple
 
-let titulo = document.querySelector(".Pruebajava")
-titulo.innerHTML = contenidoTitulo
+//let titulo = document.querySelector(".Pruebajava")
+//titulo.innerHTML = contenidoTitulo
 
 //CONDICIONALES
 
-let textoTitulo = titulo.innerText
-console.log(textoTitulo);
+//let textoTitulo = titulo.innerText
+//console.log(textoTitulo);
 
-if(textoTitulo == "Acerca de mí"){
-    titulo.innerHTML = "Otro";
-} else {
-    console.log("no se cumple");
-}
+//if(textoTitulo == "Acerca de mí"){
+    //titulo.innerHTML = "Otro";
+//} else {
+   // console.log("no se cumple");
+//}
 
 //web sin titulo reivsar condicional de otra manera
 
@@ -35,23 +35,23 @@ if(textoTitulo == "Acerca de mí"){
 //FUNCIONES
 
 //let nombre = "El wisho"
-let ciudad = "Groelandia"
-let gusto  = "chocolate"
+//let ciudad = "Groelandia"
+//let gusto  = "chocolate"
 
-let parrafo = document.querySelector(".parrafo2");
+//let parrafo = document.querySelector(".parrafo2");
 
-function cambiarTexto(nombre, ciudad, gusto){
-    let contenido = `Me llamo ${nombre}, soy de ${ciudad} y vivo actualmente en Lima. 
-    Me gusta ${gusto}, jugar videojuegos, los juegos de mesa como ajedrez, leer 
-    y platicar con amigos. Prefiero los climas frios y lluviosos (que morirme de calor). 
-    Soy un jugador casual, (gamer de corazón - manco de profesión) y estoy aprendiendo 
-    programacion ya que considero que es super útil, y es el idioma del futuro. Me gusta
-    lo que he aprendido hasta el momento y espero seguir aprendiendo mucho mas.`;
+//function cambiarTexto(nombre, ciudad, gusto){
+    //let contenido = `Me llamo ${nombre}, soy de ${ciudad} y vivo actualmente en Lima. 
+    //Me gusta ${gusto}, jugar videojuegos, los juegos de mesa como ajedrez, leer 
+    //y platicar con amigos. Prefiero los climas frios y lluviosos (que morirme de calor). 
+    //Soy un jugador casual, (gamer de corazón - manco de profesión) y estoy aprendiendo 
+   // programacion ya que considero que es super útil, y es el idioma del futuro. Me gusta
+    //lo que he aprendido hasta el momento y espero seguir aprendiendo mucho mas.`;
 
-    return contenido;
-}
+    //return contenido;
+//}
 
-parrafo.innerText = cambiarTexto(nombre, ciudad, gusto);
+//parrafo.innerText = cambiarTexto(nombre, ciudad, gusto);
 
 //PRUEBA HAMBURGUESA
 
